24d3332b5040fabe8d93f84921ea0da6750ba13a
barleytea <barleytea362@gmail.com>
2023-10-23 12:43:57 +0900

    :sparkles: Implement command to generate post template

---
+++ postgen.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+CREATED=$(date '+%Y%m%d')
+CREATED_HYPHEN=$(date '+%Y-%m-%d')
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+read -p 'Enter post title: ' title
+if [ -z "$title" ]; then
+  echo "Title required"
+  exit 1
+fi
+
+read -p 'Enter post slug: ' slug
+if [ -z "$slug" ]; then
+  echo "Slug required"
+  exit 1
+fi
+
+mkdir -p "./src/contents/${CREATED}-${slug}"
+cd $_
+
+cat << EOT >> index.md
+---
+title: "$title"
+created: "$CREATED_HYPHEN"
+path: "/${slug}"
+eyecatcher: "./eyecatcher.png"
+tags: []
+---
+EOT
