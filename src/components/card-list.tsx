42ae8bb7e60672a86bb63e172582dc3930e2194d
barleytea <barleytea362@gmail.com>
2023-10-30 21:33:10 +0900

    :iphone: Responsive design

--- src/components/card-list.tsx
+++ src/components/card-list.tsx
@@ -5,7 +5,7 @@ export const CardList: ComponentType<{
   nodes: Queries.paginationQuery['allMarkdownRemark']['nodes']
 }> = ({ nodes }) => {
   return (
-    <ul className="grid list-none grid-cols-3 gap-1 pl-0">
+    <ul className="xs:grid-cols-2 grid list-none gap-1 pl-0 sm:grid-cols-2 md:grid-cols-3">
       {nodes.map((node) => (
         <li key={node.id} className="rounded border border-gray-500">
           <Card node={node} />
