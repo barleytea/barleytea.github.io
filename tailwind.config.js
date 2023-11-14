edc73b90a06b03cc5f2288450b311674b19ec764
barleytea <barleytea362@gmail.com>
2023-11-14 13:25:19 +0900

    :rotating_light: Format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
-import defaultThemes from 'tailwindcss/defaultTheme';
+import defaultThemes from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
-export const content = ['./src/**/*.{js,ts,jsx,tsx}'];
+export const content = ['./src/**/*.{js,ts,jsx,tsx}']
 export const theme = {
   screens: {
-    ...defaultThemes.screens
+    ...defaultThemes.screens,
   },
   extend: {},
-};
+}
 export const plugins = [
   require('@tailwindcss/typography'),
   require('@tailwindcss/line-clamp'),
-];
+]
