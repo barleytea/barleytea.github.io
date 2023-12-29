3c0061d83126c77caf6d5a94ba2427cf9ec5a51f
barleytea <barleytea362@gmail.com>
2023-12-29 15:49:33 +0900

    :recycle: Fix qraphql query name

--- src/components/card-list.tsx
+++ src/components/card-list.tsx
@@ -2,7 +2,7 @@ import { ComponentType } from 'react'
 import { Card } from './card'
 
 export const CardList: ComponentType<{
-  nodes: Queries.paginationQuery['allMarkdownRemark']['nodes']
+  nodes: Queries.PaginationQuery['allMarkdownRemark']['nodes']
 }> = ({ nodes }) => {
   return (
     <ul className="xs:grid-cols-2 grid list-none gap-1 pl-0 sm:grid-cols-2 md:grid-cols-3">
